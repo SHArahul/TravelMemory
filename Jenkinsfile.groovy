8097d90ee0e702d8abf138a5d49ea43688ee70f5
Rahul Sharma <rsharma3639@gmail.com>
2025-09-07 13:04:13 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/SHArahul/TravelMemory.git'
+            }
+        }
+        stage('Install') {
+            steps {
+                sh 'cd backend; npm install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'cd backend; npm run build'
+            }
+        }
+    }
+
+}
+
